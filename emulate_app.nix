01a4657141aa993fb8ea0026e176515a6cb56091
Maix0 <maix522@gmail.com>
2023-07-21 18:30:13 +0200

    working examples

--- emulate_app.nix
+++ emulate_app.nix
@@ -13,57 +13,59 @@
 in
   pkgs.writeScriptBin "run-test-emulator"
   ''
-        #!${pkgs.runtimeShell} -e
+    #!${pkgs.runtimeShell} -e
 
-        # We need a TMPDIR
-        if [ "$TMPDIR" = "" ]
+    echo "$0"
+
+    # We need a TMPDIR
+    if [ "$TMPDIR" = "" ]
+    then
+        export TMPDIR=/tmp
+    fi
+
+    # Store the virtual devices somewhere else, instead of polluting a user's HOME directory
+    export ANDROID_USER_HOME=$(mktemp -d $TMPDIR/nix-android-user-home-XXXX)
+
+
+    export ANDROID_AVD_HOME=$ANDROID_USER_HOME/avd
+
+    # We need to specify the location of the Android SDK root folder
+    export ANDROID_SDK_ROOT=${sdk}/libexec/android-sdk
+
+
+    # We have to look for a free TCP port
+
+    echo "Looking for a free TCP port in range 5554-5584" >&2
+
+    for i in $(seq 5554 2 5584)
+    do
+        if [ -z "$(${sdk}/bin/adb devices | grep emulator-$i)" ]
         then
-            export TMPDIR=/tmp
+            port=$i
+            break
         fi
+    done
 
-        # Store the virtual devices somewhere else, instead of polluting a user's HOME directory
-        export ANDROID_USER_HOME=$(mktemp -d $TMPDIR/nix-android-user-home-XXXX)
+    if [ -z "$port" ]
+    then
+        echo "Unfortunately, the emulator port space is exhausted!" >&2
+        exit 1
+    else
+        echo "We have a free TCP port: $port" >&2
+    fi
 
+    export ANDROID_SERIAL="emulator-$port"
 
-        export ANDROID_AVD_HOME=$ANDROID_USER_HOME/avd
+    # ${sdk}/bin/sdkmanager --list
 
-        # We need to specify the location of the Android SDK root folder
-        export ANDROID_SDK_ROOT=${sdk}/libexec/android-sdk
+    # Create a virtual android device for testing if it does not exist
+    ${sdk}/bin/avdmanager list target
 
-
-        # We have to look for a free TCP port
-
-        echo "Looking for a free TCP port in range 5554-5584" >&2
-
-        for i in $(seq 5554 2 5584)
-        do
-            if [ -z "$(${sdk}/bin/adb devices | grep emulator-$i)" ]
-            then
-                port=$i
-                break
-            fi
-        done
-
-        if [ -z "$port" ]
-        then
-            echo "Unfortunately, the emulator port space is exhausted!" >&2
-            exit 1
-        else
-            echo "We have a free TCP port: $port" >&2
-        fi
-
-        export ANDROID_SERIAL="emulator-$port"
-
-        ${sdk}/bin/sdkmanager --list
-
-        # Create a virtual android device for testing if it does not exist
-        ${sdk}/bin/avdmanager list target
-
-        if [ "$(${sdk}/bin/avdmanager list avd | grep 'Name: device')" = "" ]
-        then
-            echo "Creating a new device"
-            # Create a virtual android device
-            yes "" | ${sdk}/bin/avdmanager create avd --force -n device -k "system-images;android-${
+    if [ "$(${sdk}/bin/avdmanager list avd | grep 'Name: device')" = "" ]
+    then
+        echo "Creating a new device"
+        # Create a virtual android device
+        yes "" | ${sdk}/bin/avdmanager create avd --force -n device -k "system-images;android-${
       builtins.head sdkArgs.platformVersions
     };${
       builtins.head sdkArgs.systemImageTypes
@@ -71,55 +73,79 @@ in
       builtins.head sdkArgs.abiVersions
     }" -p $ANDROID_AVD_HOME $NIX_ANDROID_AVD_FLAGS
 
-        fi
+    fi
 
-        # Launch the emulator
-        echo -e "\nLaunch the emulator"
-        $ANDROID_SDK_ROOT/emulator/emulator -avd device -no-boot-anim -port $port $NIX_ANDROID_EMULATOR_FLAGS &
+    # Launch the emulator
+    echo -e "\nLaunch the emulator"
+    $ANDROID_SDK_ROOT/emulator/emulator -avd device -no-boot-anim -port $port $NIX_ANDROID_EMULATOR_FLAGS &
 
-        # Wait until the device has completely booted
-        echo "Waiting until the emulator has booted the device and the package manager is ready..." >&2
+    # Wait until the device has completely booted
+    echo "Waiting until the emulator has booted the device and the package manager is ready..." >&2
 
-        ${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port wait-for-device
+    ${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port wait-for-device
 
-        echo "Device state has been reached" >&2
+    echo "Device state has been reached" >&2
 
-        while [ -z "$(${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port shell getprop dev.bootcomplete | grep 1)" ]
-        do
-            sleep 5
-        done
+    while [ -z "$(${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port shell getprop dev.bootcomplete | grep 1)" ]
+    do
+        sleep 5
+    done
 
-        echo "dev.bootcomplete property is 1" >&2
-
-        #while [ -z "$(${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port shell getprop sys.boot_completed | grep 1)" ]
-        #do
-            #sleep 5
-        #done
-    é
-        #echo "sys.boot_completed property is 1" >&2
-
-        echo "ready" >&2
-
-        # Install the App through the debugger, if it has not been installed yet
+    echo "dev.bootcomplete property is 1" >&2
 
 
-        ${
-      builtins.concatStringsSep "\n" (map (app: ''
+    echo "ready" >&2
+
+    # Install the App through the debugger, if it has not been installed yet
+
+
+    ${
+      builtins.concatStringsSep "\n" (map (app:
+        if app.passthru ? apkLocation && builtins.isString app.passthru.apkLocation
+        then ''
+          if [ "$(${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port shell pm list packages | grep package:${app.passthru.packageName})" = "" ]
+          then
+              ${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port install "${app}/${app.passthru.apkLocation}"
+          fi
+        ''
+        else if app.passthru ? apkLocation && builtins.isList app.passthru.apkLocation
+        then
+          builtins.concatStringsSep "\n" (map (apkPath:
+            if builtins.isString (apkPath ? null)
+            then ''
+              if [ "$(${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port shell pm list packages | grep package:${app.passthru.packageName})" = "" ]
+              then
+                  ${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port install "${app}/${apkPath}"
+              fi
+            ''
+            else builtins.throw ''Given "apkPath" isn't a string. came from derivation "${
+                if app ? name
+                then app.name
+                else "<no-name>"
+              }" (${app})''))
+          app.passthru.apkLocation
+        else ''
           if [ "$(${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port shell pm list packages | grep package:${app.passthru.packageName})" = "" ]
           then
               if [ -d "${app}" ]
               then
-                  appPath="$(echo ${app}/*.apk)"
+                  for apkFile in ${app}/*.apk; do
+                    ${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port install "$apkFile"
+                  done;
               else
-                  appPath="${app}"
+                  ${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port install "${app}"
               fi
-
-              ${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port install "$appPath"
           fi
         '')
-        installed)
+      installed)
     }
 
-        # Start the application
-        ${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port shell am start -a android.intent.action.MAIN -n ${runPackage}/${runActivity} ${runFlags}
+    # Start the application
+    ${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port shell am start -a android.intent.action.MAIN -n ${runPackage}/${runActivity} ${runFlags}
+
+    echo "Waiting for the device to stop"
+    while ${sdk}/libexec/android-sdk/platform-tools/adb -s emulator-$port get-state 1>/dev/null 2>/dev/null;
+    do
+        sleep 5
+    done
   ''
--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {
-          system = system;
+          inherit system;
           config = {
             allowUnfree = true;
             android_sdk.accept_license = true;
@@ -32,131 +32,130 @@
             buildToolsVersions = ["30.0.3"];
             platformVersions = ["33"];
             abiVersions = ["arm64-v8a"];
-            systemImageTypes = ["default"];
+            systemImageTypes = ["google_apis"];
             cmdLineToolsVersion = "8.0";
           }
           // {
             includeEmulator = true;
             includeSystemImages = true;
-            /*
-              includeExtras = [
-              "system-images;android-${
-                builtins.head defaultSdkArgs.platformVersions
-              };${
-                builtins.head defaultSdkArgs.systemImageTypes
-              };${
-                builtins.head defaultSdkArgs.abiVersions
-              }"
-            ];
-            */
           };
 
-        buildExtension = (
-          {
-            gradleTask,
-            name,
-            packageName,
-            buildType ? "debug",
-            sdkArgs ? defaultSdkArgs,
-          }
-          : (
-            let
-              sdk = (pkgs.androidenv.composeAndroidPackages sdkArgs).androidsdk;
-              ANDROID_SDK_ROOT = "${sdk}/libexec/android-sdk";
-              patched_repository = pkgs.stdenv.mkDerivation {
-                name = "repository-aapt2-patched";
-                src = repository;
-                buildPhase = with pkgs; ''
-                  export CUR_DIR=$(pwd)
-                  cd com/android/tools/build/aapt2/*/
-                  ${unzip}/bin/unzip *.jar aapt2
-                  ${patchelf}/bin/patchelf aapt2 --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)"
-                  ${zip}/bin/zip -r *.jar aapt2
-                  rm aapt2
-                  cd "$CUR_DIR"
-                '';
-                installPhase = ''
-                  mkdir -p $out
-                  mv * $out/
-                '';
-              };
-
-              repo_code = ''
-                maven {url = uri("file://" + "${patched_repository}") }
+        buildExtension = {
+          gradleTask,
+          name,
+          packageName,
+          buildType ? "debug",
+          sdkArgs ? defaultSdkArgs,
+        }
+        : (
+          let
+            sdk = (pkgs.androidenv.composeAndroidPackages sdkArgs).androidsdk;
+            ANDROID_SDK_ROOT = "${sdk}/libexec/android-sdk";
+            patched_repository = pkgs.stdenv.mkDerivation {
+              name = "repository-aapt2-patched";
+              src = repository;
+              buildPhase = with pkgs; ''
+                export CUR_DIR=$(pwd)
+                cd com/android/tools/build/aapt2/*/
+                ${unzip}/bin/unzip *.jar aapt2
+                ${patchelf}/bin/patchelf aapt2 --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)"
+                ${zip}/bin/zip -r *.jar aapt2
+                rm aapt2
+                cd "$CUR_DIR"
               '';
-            in
-              pkgs.stdenv.mkDerivation {
-                inherit name;
-                src = tachiyomi-extensions;
-                buildInputs = with pkgs; [gradle sdk openjdk kotlin fastmod ripgrep];
-                unpackCmd = ''
-                  cp -r $curSrc .
-                '';
+              installPhase = ''
+                mkdir -p $out
+                mv * $out/
+              '';
+            };
 
-                passthru = {inherit packageName;};
+            repo_code = ''
+              maven {url = uri("file://" + "${patched_repository}") }
+            '';
+          in
+            pkgs.stdenv.mkDerivation {
+              inherit name;
+              src = tachiyomi-extensions;
+              buildInputs = with pkgs; [gradle sdk openjdk kotlin fastmod ripgrep];
+              unpackCmd = ''
+                cp -r $curSrc .
+              '';
 
-                buildPhase = with pkgs; ''
-                  export ANDROID_SDK_ROOT="${ANDROID_SDK_ROOT}"
-                  export ANDROID_NDK_ROOT="${ANDROID_SDK_ROOT}/ndk-bundle";
-                  export PATH="${ANDROID_SDK_ROOT}/build-tools/${builtins.head sdkArgs.buildToolsVersions}:$PATH"
-                  export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${builtins.head sdkArgs.buildToolsVersions}/aapt2"
-                  export GRADLE_USER_HOME="$(mktemp -d)"
+              passthru = {inherit packageName;};
 
-
-
-                  echo "Fixing Repositories"
-                  ${
-                    builtins.concatStringsSep "\n" (builtins.map (
-                        pat: ''
-                          ${fastmod}/bin/fastmod --accept-all '${pat}' '${repo_code}'
-                        ''
-                      )
-                      [''maven\(.*?\)'' ''mavenCentral\(\)'' ''google\(\)''])
-                  }
-                  echo "Fixed Repositories"
-
-                  echo "Fixing Plugins"
-                  ${fastmod}/bin/fastmod '`(.*?)`' '"${"$" + "{1}"}"' -e "kts,gradle" --accept-all
-                  echo "Fixed Plugins"
-
-                  echo "Fixing Android Config in .kts files"
-
-                  export _DATA="$(${ripgrep}/bin/rg 'const val (\w+) = (.*)$' --replace "\$1 0000PLEASE_SWAP_WITH_AN_NEWLINE0000 \$2" ./buildSrc/src/main/kotlin/AndroidConfig.kt | sed -e "s/0000PLEASE_SWAP_WITH_AN_NEWLINE0000/\n/" | sed 's/^ *//;s/ *$//')"
-
-                  echo "$_DATA" | while read -r name && read -r value; do
-                    ${fastmod}/bin/fastmod -F "AndroidConfig.$name" "($value)" -e "kts,gradle" --accept-all
-                  done;
-
-                  echo "Fixed Android Config in .kts files"
+              buildPhase = with pkgs; ''
+                export ANDROID_SDK_ROOT="${ANDROID_SDK_ROOT}"
+                export ANDROID_NDK_ROOT="${ANDROID_SDK_ROOT}/ndk-bundle";
+                export PATH="${ANDROID_SDK_ROOT}/build-tools/${builtins.head sdkArgs.buildToolsVersions}:$PATH"
+                export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${builtins.head sdkArgs.buildToolsVersions}/aapt2"
+                export GRADLE_USER_HOME="$(mktemp -d)"
 
 
-                  echo "Generating Extension code"
-                  ${gradle}/bin/gradle --no-daemon --console plain multisrc:generateExtensions || exit 1;
-                  echo "Generated Extension code"
-                  echo "Building Apk"
-                  ${gradle}/bin/gradle --no-daemon --console plain "${gradleTask}"
-                '';
-                installPhase = ''
-                  mkdir -p $out
-                  mv ./generated-src/en/*/build/outputs/apk/${buildType}/*.apk $out/ || true
-                '';
-              }
-          )
+
+                echo "Fixing Repositories"
+                ${
+                  builtins.concatStringsSep "\n" (builtins.map (
+                      pat: ''
+                        ${fastmod}/bin/fastmod --accept-all '${pat}' '${repo_code}'
+                      ''
+                    )
+                    [''maven\(.*?\)'' ''mavenCentral\(\)'' ''google\(\)''])
+                }
+                echo "Fixed Repositories"
+
+                echo "Fixing Plugins"
+                ${fastmod}/bin/fastmod '`(.*?)`' '"${"$" + "{1}"}"' -e "kts,gradle" --accept-all
+                echo "Fixed Plugins"
+
+                echo "Fixing Android Config in .kts files"
+
+                export _DATA="$(${ripgrep}/bin/rg 'const val (\w+) = (.*)$' --replace "\$1 0000PLEASE_SWAP_WITH_AN_NEWLINE0000 \$2" ./buildSrc/src/main/kotlin/AndroidConfig.kt | sed -e "s/0000PLEASE_SWAP_WITH_AN_NEWLINE0000/\n/" | sed 's/^ *//;s/ *$//')"
+
+                echo "$_DATA" | while read -r name && read -r value; do
+                  ${fastmod}/bin/fastmod -F "AndroidConfig.$name" "($value)" -e "kts,gradle" --accept-all
+                done;
+
+                echo "Fixed Android Config in .kts files"
+
+
+                echo "Generating Extension code"
+                ${gradle}/bin/gradle --no-daemon --console plain multisrc:generateExtensions || exit 1;
+                echo "Generated Extension code"
+                echo "Building Apk"
+                ${gradle}/bin/gradle --no-daemon --console plain "${gradleTask}"
+              '';
+              installPhase = ''
+                mkdir -p $out
+                mv ./generated-src/en/*/build/outputs/apk/${buildType}/*.apk $out/ || true
+              '';
+            }
         );
+        tachiyomi-apk-renamed = pkgs.stdenv.mkDerivation {
+          name = "tachiyomi-apk-renamed";
+          src = tachiyomi-apk;
+          passthru.packageName = "eu.kanade.tachiyomi";
+          passthru.apkLocation = "out.apk";
+          dontUnpack = true;
+
+          installPhase = ''
+            mkdir -p $out/
+            cp ${tachiyomi-apk} "$out/out.apk"
+          '';
+        };
       in rec {
         lib = {
-          defaultSdkArgs = defaultSdkArgs;
-          buildExtension = buildExtension;
+          inherit defaultSdkArgs buildExtension;
           runEmulator = import ./emulate_app.nix {inherit pkgs defaultSdkArgs;};
         };
         apps = {
           enryu-extension = flake-utils.lib.mkApp {
             drv = lib.runEmulator {
               name = "enryu-emulator";
-              installed = [(tachiyomi-apk // {passthru.packageName = "eu.kanade.tachiyomi";}) packages.enryu-apk];
+              installed = [tachiyomi-apk-renamed packages.enryu-apk];
               runPackage = "eu.kanade.tachiyomi";
               runActivity = "eu.kanade.tachiyomi.ui.main.MainActivity";
               runFlags = "-a eu.kanade.tachiyomi.SHOW_CATALOGUES";
+              sdkArgs = defaultSdkArgs // {abiVersions = ["x86_64"];};
             };
           };
         };
